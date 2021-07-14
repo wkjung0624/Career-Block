e55ea37a8916bde6251a70230af5da9e62638b61
wkjung0624 <>
2021-07-14 17:55:12 +0900

    Add Login Page

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,11 +1,13 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import {
+  createRouter,
+  createWebHistory
+} from "vue-router";
+import Login from "../components/Login.vue";
 
-const routes = [
-  {
+const routes = [{
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Login",
+    component: Login,
   },
   {
     path: "/about",
@@ -14,7 +16,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import( /* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
