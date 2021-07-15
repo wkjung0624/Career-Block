b7d78b3c0a44208283e3ff95acf89d3857290705
wkjung0624 <skyship36@gmail.com>
2021-07-15 23:14:20 +0900

    add: Add CSS for navigate bar items

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="nav-title">CareerBlock</div>
+    <router-link to="/agree" class="nav-item">피드백</router-link>
+    <router-link to="/education" class="nav-item">링크생성</router-link>
+    <router-link to="/personal" class="nav-item">기본정보</router-link>
+    <router-link to="/" class="nav-item">이력서생성</router-link>
   </div>
   <router-view />
+
 </template>
 
 <style>
@@ -16,7 +20,9 @@
 }
 
 #nav {
-  padding: 30px;
+  height:50px;
+  padding:20px 30px 0px 30px;
+  background: rgb(22, 22, 22);
 }
 
 #nav a {
@@ -26,5 +32,17 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.nav-title{
+  font-size:30px;
+  color:RGB(255,255,255);
+  float:left;
+}
+
+.nav-item{
+  font-size:25px;
+  color:RGB(255,255,255);
+  float:right;
 }
 </style>
