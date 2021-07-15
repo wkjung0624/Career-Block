581dc0ffd5b5e2048ab28d754a0c3033a831c26a
wkjung0624 <skyship36@gmail.com>
2021-07-15 23:48:38 +0900

    update: Update 'Learn.vue' component to index.js

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -57,6 +57,15 @@ const routes = [
     component: () =>
       import( /* webpackChunkName: "about" */ "../components/Education.vue"),
   },
+  {
+    path: "/learn",
+    name: "Learn",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( /* webpackChunkName: "about" */ "../components/Learn.vue"),
+  },
 ];
 
 const router = createRouter({
