0c8e23519ace4e53c1d4ede725fbbebac3ad0514
Shumay14 <seo533481@naver.com>
2021-07-19 15:41:06 +0900

    regi & home

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,25 +1,27 @@
 <template>
- <div class="bg-main">
-  오신걸환영합니다
-        <button @click="regsw(true)">ron</button>
-        <button @click="regsw(false)">roff</button>
-        
-  <div v-if="!isLogin">
-    <Login />
-  </div>
-  <div v-if="isLogin && isRegister">
+  <div class="container">
+    <br /><br />
+    <div style="font-size: 30pt; color: #867ae9">Welcome to CareerBlock!!</div>
+    <br /><br />
+    <button @click="regsw(true)" style="font-size: 15pt">resume on</button>
+    <button @click="regsw(false)" style="font-size: 15pt">resume off</button>
+    <br /><br />
+
+    <div v-if="!isLogin">
+      <Login />
+    </div>
+    <div v-if="isLogin && isRegister">
       <Profile />
       <Education />
       <Career />
       <Certification />
       <Learn />
-  </div>
-  <div v-if="isLogin && !isRegister">
-    우리 사이트가 처음이세요 ?
-    <a href="/registration"> 가입하러가기 </a>
-    <!-- <Registration /> -->
-  </div>
-    
+    </div>
+    <div v-if="isLogin && !isRegister" style="font-size: 15pt">
+      우리 사이트가 처음이세요 ?
+      <a href="/registration"> 가입하러가기 </a>
+      <!-- <Registration /> -->
+    </div>
   </div>
 </template>
 
@@ -36,41 +38,51 @@ import Registration from "@/views/Registration.vue";
 export default {
   name: "components",
   components: {
-    Profile, Education, Learn, Certification, Career, Login, Registration
+    Profile,
+    Education,
+    Learn,
+    Certification,
+    Career,
+    Login,
+    Registration,
   },
-  data(){
-    return{
-      testing : 0,
+  data() {
+    return {
+      testing: 0,
     };
   },
-  computed:{
-    isLogin(){
+  computed: {
+    isLogin() {
       return this.$store.state.user.email;
     },
-    isRegister(){
+    isRegister() {
       return this.$store.state.user.isRegister;
     },
   },
   methods: {
-        regsw(param){
-            var test = this.$store.state.user.email == "";
-            console.log(test, this.$store.state.user.email)
+    regsw(param) {
+      var test = this.$store.state.user.email == "";
+      console.log(test, this.$store.state.user.email);
 
-            this.$store.state.user.isRegister = param
-            
-            console.log("isReg:",this.$store.state.user.isRegister);
-            console.log(this.$store.state.user);
-        }, 
-  }
+      this.$store.state.user.isRegister = param;
+
+      console.log("isReg:", this.$store.state.user.isRegister);
+      console.log(this.$store.state.user);
+    },
+  },
 };
 </script>
 
-
 <style scoped>
-  .bg-main{
-        padding: 50px;
-        width:1000px;
-        height:2000px;
-        background-color: RGB(134,122,233);
-    }
+@import url(//fonts.googleapis.com/earlyaccess/hanna.css);
+* {
+  box-sizing: border-box;
+  font-family: "Hanna", sans-serif;
+}
+.container {
+  /* padding: 50px; */
+  width: 100%;
+  height: 2000px;
+  background-color: aliceblue;
+}
 </style>
--- front-end/src/views/Registration.vue
+++ front-end/src/views/Registration.vue
@@ -1,15 +1,16 @@
 <template>
-    <div v-if="true">
-        <Agree @send-message="sendMessage" v-if="checkSteps() == 0"/>
-        <Personal @send-message="sendMessage" v-if="checkSteps() == 1"/>
-        <div v-if="checkSteps() == 2">
-            회원가입이 완료되었습니다~
-            <a href="/">이용하러가기</a>
-        </div>
+  <div class="container">
+    <br /><br />
+    <div v-if="true" style="font-size: 15pt">
+      <Agree @send-message="sendMessage" v-if="checkSteps() == 0" />
+      <Personal @send-message="sendMessage" v-if="checkSteps() == 1" />
+      <div v-if="checkSteps() == 2">
+        회원가입이 완료되었습니다~
+        <a href="/">이용하러가기</a>
+      </div>
     </div>
-    <div v-else>
-        먼저 로그인 해주세요.
-    </div>
+    <div v-else style="font-size: 15pt">먼저 로그인 해주세요.</div>
+  </div>
 </template>
 
 <script>
@@ -17,37 +18,50 @@ import Personal from "@/components/Personal.vue";
 import Agree from "@/components/Agree.vue";
 
 export default {
-    components: {
-        Agree, Personal
+  components: {
+    Agree,
+    Personal,
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
     },
-    computed:{
-        user(){
-            return this.$store.state.user;
-        },
-    },
-    
-    data(){
-        return {
-            steps : 0,
-        }
-    },
-    created(){
-        console.log("hi",  this.$store.state.user);
-    },
-    methods:{
-        checkSteps(){
-            if(this.steps == 2){
-                this.$store.commit("Regt");
-            }
+  },
 
-            return this.steps;
-        },
-        sendMessage(data) {
-            console.log("호출",data);
-            this.steps = data
-        }
-    }
-}
+  data() {
+    return {
+      steps: 0,
+    };
+  },
+  created() {
+    console.log("hi", this.$store.state.user);
+  },
+  methods: {
+    checkSteps() {
+      if (this.steps == 2) {
+        this.$store.commit("Regt");
+      }
+
+      return this.steps;
+    },
+    sendMessage(data) {
+      console.log("호출", data);
+      this.steps = data;
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+@import url(//fonts.googleapis.com/earlyaccess/hanna.css);
+* {
+  box-sizing: border-box;
+  font-family: "Hanna", sans-serif;
+}
+.container {
+  /* padding: 50px; */
+  width: 100%;
+  height: 2000px;
+  background-color: aliceblue;
+}
+</style>
