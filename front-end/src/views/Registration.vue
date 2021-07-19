edf482e93c2548b6973b32e74c659c55ead0fcb6
Shumay14 <seo533481@naver.com>
2021-07-19 16:36:23 +0900

    home regi edit

--- front-end/src/views/Registration.vue
+++ front-end/src/views/Registration.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container">
     <br /><br />
+
+    <div style="font-size: 50pt; color: #867ae9">CareerBlock</div>
+    <br /><br />
+
     <div v-if="true" style="font-size: 15pt">
       <Agree @send-message="sendMessage" v-if="checkSteps() == 0" />
       <Personal @send-message="sendMessage" v-if="checkSteps() == 1" />
       <div v-if="checkSteps() == 2">
-        회원가입이 완료되었습니다~
+        <span style="font-size: 15pt; color: navy">커리어블록</span>
+        회원이 되신 것을 축하드립니다. :)
+
+        <br /><br />
+
+        <div>
+          커리어블록과 함께 <b>경력</b>과 <b>이력서</b>를 편리하게 관리하세요.
+        </div>
+
+        <br /><br />
+
         <a href="/">이용하러가기</a>
       </div>
     </div>
