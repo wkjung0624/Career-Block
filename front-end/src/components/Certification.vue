25c32511fae3a2fcf8e5afd03e2606db0ea76d04
Shumay14 <seo533481@naver.com>
2021-07-19 11:44:31 +0900

    done

--- front-end/src/components/Certification.vue
+++ front-end/src/components/Certification.vue
@@ -3,41 +3,43 @@
     <br />
     <div style="font-size: 30pt; color: white">자격증</div>
     <br /><br />
-    <button @click="changeEditable()">수정가능상태 변경</button>
-    <button @click="showDataInfo()">입력값 콘솔 출력</button>
+    <button @click="changeEditable()" class="d3">수정가능상태 변경</button>
+    <button @click="showDataInfo()" class="d3">입력값 콘솔 출력</button>
     <br /><br />
     <div v-bind:key="index" v-for="(item, index) in CerList" class="container">
-      <div class="row" v-if="isEditable">
+      <div v-if="isEditable">
         <div class="d1">
-          <div>
+          <div class="d2">
             취득년월<br />
             <input type="text" v-model="item.CerDate" class="tbox" />
           </div>
-          <div>
+          <div class="d2">
             자격증명<br />
             <input type="text" v-model="item.CerName" class="tbox" />
           </div>
-          <div>
+          <div class="d2">
             발급기관<br />
             <input type="text" v-model="item.CerIns" class="tbox" />
           </div>
 
           <!-- 열삭제 -->
-          <div><button class="btn" @click="delCer">삭제</button><br /></div>
+          <div class="d3">
+            <button class="btn" @click="delCer">삭제</button><br />
+          </div>
         </div>
       </div>
 
-      <div class="row" v-else>
+      <div v-else>
         <div class="d1">
-          <div>
+          <div class="d2">
             취득년월<br />
             {{ item.CerDate }}
           </div>
-          <div>
+          <div class="d2">
             자격증명<br />
             {{ item.CerName }}
           </div>
-          <div>
+          <div class="d2">
             발급기관<br />
             {{ item.CerIns }}
           </div>
@@ -45,7 +47,7 @@
       </div>
     </div>
 
-    <label style="padding-left: 500px">
+    <label class="d4">
       <!-- 열추가 -->
       <button class="btn2" @click="addCer">추가</button>
     </label>
@@ -108,43 +110,56 @@ export default {
   /* position: relative; */
   /* padding-bottom: 50px; */
   background: #867ae9;
-  /* padding: 10px; */
-  /* text-align: left; */
+  padding: 20px;
+  text-align: left;
 }
 
 .container {
   border: 1px solid #867ae9;
   background-color: aliceblue;
-  /* padding: 10px; */
+  padding: 10px;
+  /* float: left; */
   /* margin: auto; */
   /* width: 95%; */
-}
-
-.row {
-  /* margin: 10px; */
-  /* border: 2px dashed; */
 }
 
 .d1 {
   /* padding: 10px; */
   font-size: 15pt;
-  /* margin: auto; */
+  margin: auto;
+  /* display: inline; */
   /* float: left; */
   /* position: relative; */
 }
 .d2 {
-  /* padding: 10px;
-  font-size: 15pt;
-  margin: auto; */
+  padding: 10px;
+  /* font-size: 15pt; */
+  margin: auto;
+  display: inline-block;
 
   /* float: left; */
+}
+.d3 {
+  /* padding: 10px; */
+  /* font-size: 15pt; */
+  margin: auto;
+  display: inline-block;
+
+  /* float: left; */
+}
+.d4 {
+  /* padding: 10px; */
+  /* font-size: 15pt; */
+  margin: auto;
+  display: block;
+  position: relative;
 }
 .tbox {
   width: 200px;
   height: 50px;
 }
 .btn {
-  width: 30px;
+  width: 50px;
   height: 30px;
   border-color: gainsboro;
   color: #867ae9;
