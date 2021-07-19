3ad2c9f48fc3c97bbbe92d8d69c67612c2cdbb8f
Shumay14 <seo533481@naver.com>
2021-07-19 17:38:32 +0900

    search editing

--- front-end/src/components/Search.vue
+++ front-end/src/components/Search.vue
@@ -1,35 +1,91 @@
 <template>
-    <div>
-        <!-- 엔터쳐도 자동으로 실행되게 -->
-        조회할 이력서 코드 : <input type="text" placeholder="이력서 고유값" v-model="query"/> 
-        <button @click="onSearch">검색</button>
-        <button @click="showParam">검색2</button>
-    </div>
+  <div>
+    <br />
+    <!-- 엔터쳐도 자동으로 실행되게 -->
+    <span class="d1">조회할 이력서 코드 : </span>
+    <input
+      type="text"
+      placeholder="이력서 고유값"
+      v-model="query"
+      class="tbox"
+    />
+    <button @click="onSearch" class="btn">검색</button>
+    <button @click="showParam" class="btn">검색2</button>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-    path: '/search',
-    data(){
-        return {
-            query : "",
-        }
+  path: "/search",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    async onSearch() {
+      // res = await axios.get('https://httpbin.org/get')
+      await axios.get("http://localhost:8081/search?r=" + this.query);
+      alert(this.query);
     },
-    methods:{
-        async onSearch(){
-            // res = await axios.get('https://httpbin.org/get')
-            await axios.get("http://localhost:8081/search?r="+this.query);
-            alert(this.query);
-        },
-        showParam(){
-            console.log(this.$route.query.r);
-        }
-    }
-    
-}
+    showParam() {
+      console.log(this.$route.query.r);
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+@import url(//fonts.googleapis.com/earlyaccess/hanna.css);
+* {
+  box-sizing: border-box;
+  font-family: "Hanna", sans-serif;
+  /* font: 12px "맑은 고딕", Malgun Gothic, "돋움", Dotum, sans-serif; */
+}
 
+.base {
+  /* overflow: hidden; */
+  /* position: relative; */
+  /* padding-bottom: 50px; */
+  background: #867ae9;
+  padding: 20px;
+  text-align: left;
+}
+
+.container {
+  border: 1px solid #867ae9;
+  background-color: aliceblue;
+  padding: 10px;
+  /* float: left; */
+  /* margin: auto; */
+  /* width: 95%; */
+}
+.d1 {
+  /* padding: 10px; */
+  font-size: 15pt;
+  margin: auto;
+  /* display: inline; */
+  /* float: left; */
+  /* position: relative; */
+}
+
+.tbox {
+  width: 400px;
+  height: 50px;
+}
+.btn {
+  width: 50px;
+  height: 30px;
+  border-color: gainsboro;
+  color: #867ae9;
+  margin: 3px;
+}
+.btn2 {
+  width: 100px;
+  height: 50px;
+  border-color: gainsboro;
+  color: #867ae9;
+  margin: 3px;
+}
 </style>
