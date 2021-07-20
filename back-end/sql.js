d3dd266e52346a6d97f835495686fa42b2bd553d
wkjung0624 <>
2021-07-20 16:32:01 +0900

    Update sql.js

--- back-end/sql.js
+++ back-end/sql.js
@@ -1,21 +1,23 @@
 module.exports = {
-    testQuery: {
-      query: "select * from USER_TB",
-    },
-    list: {
-      query: "select * from t_person",
-    },
-    searchPerson: {
-      query: "select * from t_person",
-    },
-    createPerson: {
-      query: "insert into t_person set ?", // insert into t_person set first_name='John3', last_name='Doe3', email='john3@gmail.com'
-    },
-    deletePerson: {
-      query: "delete from t_person where id=?",
-    },
-    updatePerson: {
-      query: "update t_person set ? where id=?",
-    },
-  };
-  
+  createAccount: {
+    query: "insert into USER_TB set ?",
+  },
+  list: {
+    query: "select * from t_person",
+  },
+  searchPerson: {
+    query: "select * from t_person",
+  },
+  createPerson: {
+    query: "insert into t_person set ?", // insert into t_person set first_name='John3', last_name='Doe3', email='john3@gmail.com'
+  },
+  deletePerson: {
+    query: "delete from t_person where id=?",
+  },
+  updatePerson: {
+    query: "update t_person set ? where id=?",
+  },
+  // 아래 참고
+  // INSERT INTO table (a, b, c) VALUES (1,2,3)
+  // INSERT INTO table SET a=1, b=2, c=3
+};
