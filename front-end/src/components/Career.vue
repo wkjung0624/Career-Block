a79a0e98aa83fc335d6d62905d7589dac26c0807
JK-121 <kyeongmin719@gmail.com>
2021-07-20 15:47:06 +0900

    .

--- front-end/src/components/Career.vue
+++ front-end/src/components/Career.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="base">
     <!-- 수정가능(로그인시)  -->
-    <div v-if="isEditable" id="delete_box">
-      <!-- 제목 -->
-      <div class="header">
+     <div class="header">
         <div>경력</div>
       </div>
-      <div class="rows" v-bind:key="idx" v-for="(item, idx) in itemList">
+    <div v-if="isEditable" id="delete_box" style="border:1px solid gainsboro;">
+      <!-- 제목 -->
+     
+      <div class="rows" v-bind:key="idx" v-for= "(item, idx) in itemList">
         <!-- 1줄 -->
         <!-- 1-1 회사명 -->
         <div class="float1">
@@ -235,7 +236,7 @@
           <div>
             <div
               class="box_line"
-              style="width: 900px"
+              style="width: 916px"
               v-show="item.isDescription == true"
             >
               <p style="text-align: left">경력기술서</p>
@@ -470,7 +471,7 @@ export default {
 #delete_click {
   position: relative;
   left: 892px;
-  bottom: 14px;
+  bottom: 12px;
 }
 
 table {
