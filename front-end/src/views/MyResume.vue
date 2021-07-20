b5fbe8242193655f3000262110882a4efa184fd0
wkjung0624 <skyship36@gmail.com>
2021-07-21 05:29:47 +0900

    Update MyResume.vue

--- front-end/src/views/MyResume.vue
+++ front-end/src/views/MyResume.vue
@@ -1,19 +1,6 @@
 <template>
-    <div>
-        banner : Hello
-    </div>
-    <div v-if="!isLogin">
-        저희 서비스를 이용하기 위해 로그인을 해주세요.
-        <a href="/login">로그인하기</a>
-    </div>
-    <div v-else-if="!isRegister">
-        저희 서비스를 이용하기 위해 회원가입을 해주세요.
-        <a href="/registration">회원가입하기</a>
-    </div>
     
-    <!-- <div v-if="isLogin && isRegister"> -->
-    <button @click="showstat">test</button>
-    <div v-if="isLogin && isRegister">
+    <div>
         <Profile />
         <Education />
         <Career />
