d7d0d6093bb45b7077e292070883be4fa825feec
wkjung0624 <skyship36@gmail.com>
2021-07-21 05:29:54 +0900

    Update store.js

--- front-end/src/assets/store.js
+++ front-end/src/assets/store.js
@@ -8,12 +8,17 @@ const store = createStore({
   state() {
       return {
           user: {},
+          currentPage: 0,
       };
   },
   mutations: {
       user(state, data) {
           state.user = data;
           state.user.isRegister = false;
+      },
+      setPageIndex(state, data){
+          console.log("test", data);
+          state.currentPage = data;
       },
       Regf(state){
         state.user.isRegister = false;
