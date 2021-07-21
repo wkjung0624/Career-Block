5f05a3fac7740ef00fd2254bb5dc64ad9456f0c9
annawinter <khi1304@naver.com>
2021-07-21 11:48:00 +0900

    Update App.vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -231,7 +231,7 @@
 
       <!-- Footer-->
       <footer class="bg-light py-5">
-          <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2021 - Company Name</div></div>
+          <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2021 - Career Block</div></div>
       </footer>
       </div>
 
