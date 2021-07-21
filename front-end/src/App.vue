6da1ad03cc246091e4e3689908df3ae8db9421e7
wkjung0624 <>
2021-07-21 12:44:24 +0900

    add: set login page redirect event to footer button

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -223,7 +223,7 @@
                       <h2 class="text-white mt-0">We've got what you need!</h2>
                       <hr class="divider divider-light" />
                       <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
-                      <a class="btn btn-light btn-xl fs-6" href="#services">Get Started!</a>
+                      <a class="btn btn-light btn-xl fs-6" @click="changePage(3)">Get Started!</a>
                   </div>
               </div>
           </div>
