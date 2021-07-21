80271972192f6e5093276a23dc6ca6c05d186217
wkjung0624 <skyship36@gmail.com>
2021-07-21 09:29:56 +0900

    Update Registration.vue

--- front-end/src/views/Registration.vue
+++ front-end/src/views/Registration.vue
@@ -1,31 +1,32 @@
 <template>
-
-  <button @click="createAccount">test</button>
-  
-  <div v-if="!isLogin">
-      <Login />
-  </div>
-
   <div>
-    <div v-if="true" style="font-size: 15pt">
-      <Agree @send-message="sendMessage" v-if="checkSteps() == 0" />
-      <Personal @send-message="sendMessage" v-if="checkSteps() == 1" />
-      <div v-if="checkSteps() == 2">
-        <span style="font-size: 15pt; color: navy">커리어블록</span>
-        회원이 되신 것을 축하드립니다. :)
-
-        <br /><br />
-
-        <div>
-          커리어블록과 함께 <b>경력</b>과 <b>이력서</b>를 편리하게 관리하세요.
-        </div>
-
-        <br /><br />
-        이제 나를 표현하러 가볼까요?<br>
-        <a href="/myresume">이용하러가기</a>
-      </div>
+    <button @click="createAccount">test</button>
+    
+    <div v-if="!isLogin">
+        <Login />
     </div>
-    <div v-else style="font-size: 15pt">먼저 로그인 해주세요.</div>
+
+    <div>
+      <div v-if="true" style="font-size: 15pt">
+        <Agree @send-message="sendMessage" v-if="checkSteps() == 0" />
+        <Personal @send-message="sendMessage" v-if="checkSteps() == 1" />
+        <div v-if="checkSteps() == 2">
+          <span style="font-size: 15pt; color: navy">커리어블록</span>
+          회원이 되신 것을 축하드립니다. :)
+
+          <br /><br />
+
+          <div>
+            커리어블록과 함께 <b>경력</b>과 <b>이력서</b>를 편리하게 관리하세요.
+          </div>
+
+          <br /><br />
+          이제 나를 표현하러 가볼까요?<br>
+          <a href="/myresume">이용하러가기</a>
+        </div>
+      </div>
+      <div v-else style="font-size: 15pt">먼저 로그인 해주세요.</div>
+    </div>
   </div>
 </template>
 
