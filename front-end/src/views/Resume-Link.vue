93aa2a155b2c3b38835ac1f1bb4c47fc0d423d5a
JK-121 <kyeongmin719@gmail.com>
2021-07-21 18:04:15 +0900

    .

--- front-end/src/views/Resume-Link.vue
+++ front-end/src/views/Resume-Link.vue
@@ -7,22 +7,22 @@
 <form class="row gy-2 gx-3 align-items-center">
   <div class="col-auto">
     <label class="visually-hidden" for="autoSizingInput">Name</label>
-    <input type="text" class="form-control" id="autoSizingInput" placeholder="Jane Doe">
+    <input type="text" class="form-control" id="autoSizingInput">
   </div>
   <div class="col-auto">
     <label class="visually-hidden" for="autoSizingInputGroup">Username</label>
     <div class="input-group">
       <div class="input-group-text">@</div>
-      <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Username">
+      <input type="text" class="form-control" id="autoSizingInputGroup">
     </div>
   </div>
   <div class="col-auto">
     <label class="visually-hidden" for="autoSizingSelect">Preference</label>
     <select class="form-select" id="autoSizingSelect">
-      <option selected>Choose...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
+      <option selected>선택</option>
+      <option value="1">gmail.com</option>
+      <option value="2">hanmail.com</option>
+      <option value="3">naver.com</option>
     </select>
   </div>
   <div class="col-auto">
