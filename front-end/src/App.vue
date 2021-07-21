06923c2b66a17671d2fe904ffcc4609d05c7871c
wkjung0624 <>
2021-07-21 11:23:33 +0900

    Update App.vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -8,8 +8,8 @@
               <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
               <div class="collapse navbar-collapse" id="navbarResponsive">
                   <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                      <li class="nav-item"><a class="nav-link fs-5" href="#about" @click="changePage(0)">About</a></li>
-                      <li class="nav-item"><a class="nav-link fs-5" href="#services" @click="changePage(0)">Services</a></li>
+                      <li class="nav-item"><a class="nav-link fs-5" href="#services" @click="changePage(0)">About</a></li>
+                      <li class="nav-item"><a class="nav-link fs-5" href="#about" @click="changePage(0)">Services</a></li>
                       <li class="nav-item"><a class="nav-link fs-5" href="#myresume" @click="changePage(1)">Resume</a></li>
                       <li class="nav-item"><a class="nav-link fs-5" href="#resumelink" @click="changePage(2)">Links</a></li>
                       <li class="nav-item">
@@ -37,63 +37,57 @@
           <div class="container px-4 px-lg-5 h-100">
               <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                   <div class="col-lg-8 align-self-end">
-                      <h1 class="text-white font-weight-bold fs-2">Manage your Resume with a Career Block for Free</h1>
+                      <h1 class="text-white font-weight-bold">
+                      <!-- Manage your Resume with a Career Block for Free -->
+                      Manage Your Career
+                      </h1>
+                      <br>
                       <hr class="divider" />
+                      <br>
                   </div>
                   <div class="col-lg-8 align-self-baseline">
-                      <p class="text-white-75 mb-5">
-                        With a career block, you can manage your career more easily and easily. We provide you with all the functions you need. Without restrictions.
-                        
+                      <p class="text-white-75 mb-5 fs-5">
+                        <!-- With a career block, you can manage your career more easily and easily. We provide you with all the functions you need. Without restrictions. -->
+                        블록체인 기술로 신뢰할만한 이력 정보를 쉽고 간편하게 제공하며<br />
+                당신의 커리어를 기록하여 당신의 커리어 스토리를 만들어 나갈 수
+                있습니다.
                         </p>
-                      <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
+                      <a class="btn btn-primary btn-xl fs-6" href="#services">Find Out More</a>
                   </div>
               </div>
           </div>
       </header>
-      <!-- About-->
-      <section class="page-section bg-primary" id="about">
-          <div class="container px-4 px-lg-5">
-              <div class="row gx-4 gx-lg-5 justify-content-center">
-                  <div class="col-lg-8 text-center">
-                      <h2 class="text-white mt-0">We've got what you need!</h2>
-                      <hr class="divider divider-light" />
-                      <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
-                      <a class="btn btn-light btn-xl" href="#services">Get Started!</a>
-                  </div>
-              </div>
-          </div>
-      </section>
       <!-- Services-->
       <section class="page-section" id="services">
           <div class="container px-4 px-lg-5">
-              <h2 class="text-center mt-0">At Your Service</h2>
+              <h2 class="text-center mt-0">Philosophy</h2>
               <hr class="divider" />
               <div class="row gx-4 gx-lg-5">
                   <div class="col-lg-3 col-md-6 text-center">
                       <div class="mt-5">
-                          <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div>
-                          <h3 class="h4 mb-2">Sturdy Themes</h3>
+                          <div class="mb-2"><i class="bi-suit-club fs-1 text-primary"></i></div>
+                          <h3 class="h4 mb-2">Trust</h3>
                           <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
                       </div>
                   </div>
                   <div class="col-lg-3 col-md-6 text-center">
                       <div class="mt-5">
-                          <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
-                          <h3 class="h4 mb-2">Up to Date</h3>
+                          <div class="mb-2"><i class="bi-command fs-1 text-primary"></i></div>
+                          <h3 class="h4 mb-2">Clarity</h3>
                           <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
                       </div>
                   </div>
                   <div class="col-lg-3 col-md-6 text-center">
                       <div class="mt-5">
-                          <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
-                          <h3 class="h4 mb-2">Ready to Publish</h3>
+                          <div class="mb-2"><i class="bi-check2-square fs-1 text-primary"></i></div>
+                          <h3 class="h4 mb-2">Fact</h3>
                           <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
                       </div>
                   </div>
                   <div class="col-lg-3 col-md-6 text-center">
                       <div class="mt-5">
-                          <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div>
-                          <h3 class="h4 mb-2">Made with Love</h3>
+                          <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
+                          <h3 class="h4 mb-2">Connect</h3>
                           <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
                       </div>
                   </div>
@@ -102,14 +96,14 @@
       </section>
       <!-- Portfolio-->
       <div id="portfolio">
-          <div class="container-fluid p-0">
+          <div class="container-fluid p-0 mb-5">
               <div class="row g-0">
                   <div class="col-lg-4 col-sm-6">
                       <a class="portfolio-box" href="./assets/img/portfolio/fullsize/1.jpg" title="Project Name">
                           <img class="img-fluid" src="./assets/img/portfolio/thumbnails/1.jpg" alt="..." />
                           <div class="portfolio-box-caption">
                               <div class="project-category text-white-50">Category</div>
-                              <div class="project-name">Project Name</div>
+                              <div class="project-name">Create Resume</div>
                           </div>
                       </a>
                   </div>
@@ -118,7 +112,7 @@
                           <img class="img-fluid" src="./assets/img/portfolio/thumbnails/2.jpg" alt="..." />
                           <div class="portfolio-box-caption">
                               <div class="project-category text-white-50">Category</div>
-                              <div class="project-name">Project Name</div>
+                              <div class="project-name">My Resume</div>
                           </div>
                       </a>
                   </div>
@@ -127,13 +121,14 @@
                           <img class="img-fluid" src="./assets/img/portfolio/thumbnails/3.jpg" alt="..." />
                           <div class="portfolio-box-caption">
                               <div class="project-category text-white-50">Category</div>
-                              <div class="project-name">Project Name</div>
+                              <div class="project-name">Resume Share Link</div>
                           </div>
                       </a>
                   </div>
               </div>
           </div>
       </div>
+      <!-- <br><br><br><br><br> -->
       <!-- Call to action-->
       <!-- <section class="page-section bg-dark text-white">
           <div class="container px-4 px-lg-5 text-center">
@@ -219,6 +214,21 @@
               </div>
           </div>
       </section> -->
+
+      <!-- About-->
+      <section class="page-section bg-primary" id="about">
+          <div class="container px-4 px-lg-5">
+              <div class="row gx-4 gx-lg-5 justify-content-center">
+                  <div class="col-lg-8 text-center">
+                      <h2 class="text-white mt-0">We've got what you need!</h2>
+                      <hr class="divider divider-light" />
+                      <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
+                      <a class="btn btn-light btn-xl fs-6" href="#services">Get Started!</a>
+                  </div>
+              </div>
+          </div>
+      </section>
+
       <!-- Footer-->
       <footer class="bg-light py-5">
           <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2021 - Company Name</div></div>
