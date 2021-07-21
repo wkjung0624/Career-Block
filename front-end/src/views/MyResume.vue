eb0395706fd50c1167927ffcf6327a3055ee5f6c
annawinter <khi1304@naver.com>
2021-07-21 12:06:30 +0900

    Update MyResume.vue

--- front-end/src/views/MyResume.vue
+++ front-end/src/views/MyResume.vue
@@ -1,17 +1,15 @@
 <template>
-    
-    <div>
-        <Profile />
-        <Education />
-        <Career />
-        <Project />
-        <Learn />
-        <Certification />
-    </div>
+  <div class="myResume">
+    <Profile />
+    <Education />
+    <Career />
+    <Project />
+    <Learn />
+    <Certification />
+  </div>
 </template>
 
 <script>
-
 import Profile from "@/components/Profile.vue";
 import Education from "@/components/Education.vue";
 import Learn from "@/components/Learn.vue";
@@ -20,35 +18,37 @@ import Project from "@/components/Project.vue";
 import Career from "@/components/Career.vue";
 
 export default {
-    name: "components",
-    components: {
-        Profile,
-        Education,
-        Learn,
-        Certification,
-        Career,
-        Project
+  name: "components",
+  components: {
+    Profile,
+    Education,
+    Learn,
+    Certification,
+    Career,
+    Project,
+  },
+  computed: {
+    isLogin() {
+      console.log("## ", this.$store.state.user.email);
+      return typeof this.$store.state.user.email != "undefined" ? true : false;
     },
-    computed: {
-        isLogin() {
-            console.log("## ",this.$store.state.user.email);
-            return (typeof this.$store.state.user.email != "undefined" ? true : false);
-        },
-        isRegister() {
-            console.log("@@ ",this.$store.state.user.isRegister);
-            return this.$store.state.user.isRegister;
-        },
+    isRegister() {
+      console.log("@@ ", this.$store.state.user.isRegister);
+      return this.$store.state.user.isRegister;
     },
-    methods:{
-        showstat(){
-            console.log("!!! ",this.$store.state.user);
-            console.log("!!! ",this.$store.state.user.email);
-            console.log("!!! ",this.$store.state.user.isRegister);
-        }
-    }
-}
+  },
+  methods: {
+    showstat() {
+      console.log("!!! ", this.$store.state.user);
+      console.log("!!! ", this.$store.state.user.email);
+      console.log("!!! ", this.$store.state.user.isRegister);
+    },
+  },
+};
 </script>
 
 <style>
-
+.myResume {
+  margin: 150px 0px 150px 0px;
+}
 </style>
